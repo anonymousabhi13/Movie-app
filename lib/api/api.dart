c2ee5514b2d36d05791f18801137195b19940e5e
Abhilash Patel <abhilashpatel687@gmail.com>
2022-11-23 11:35:28 +0530

    fetching data and displaying

---
+++ lib/api/api.dart
@@ -0,0 +1,5 @@
+class Api {
+  static const baseUrl = "https://api.themoviedb.org/3/";
+  static const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
+  static const apiKey = "31e27d32a35acef5e047579389873b0c";
+}
